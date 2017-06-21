d1833ebcb33b85b5329977119d7631019c7cbd6d
Navendu Agarwal <navendu.agarwal@gmail.com>
2017-06-21 16:08:44 +0530

    updates

---
+++ app/src/main/java/com/example/android/classicalmusicquiz/ClassicalMusicQuiz.java
@@ -0,0 +1,45 @@
+package com.example.android.classicalmusicquiz;
+
+import android.util.Log;
+
+import timber.log.Timber;
+
+/**
+ * Application class
+ */
+
+public class ClassicalMusicQuiz extends android.app.Application{
+    @Override public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree(){
+                @Override
+                protected String createStackElementTag(StackTraceElement element) {
+                    return super.createStackElementTag(element) + ":" + element.getLineNumber();
+                }
+            });
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+    }
+
+    /** A tree which logs important information for crash reporting. */
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override protected void log(int priority, String tag, String message, Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+
+//            FakeCrashLibrary.log(priority, tag, message);
+
+            if (t != null) {
+                if (priority == Log.ERROR) {
+//                    FakeCrashLibrary.logError(t);
+                } else if (priority == Log.WARN) {
+//                    FakeCrashLibrary.logWarning(t);
+                }
+            }
+        }
+    }
+}
